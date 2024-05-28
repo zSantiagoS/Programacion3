86b46322aa66d80725db89ab95b241008f2b7eba
nelson.apache <molina123apache@gmal.com>
2024-05-28 02:26:04 -0500

    feat: Agregar lógica para comprar boletas en controlador de proceso de compra

--- ProyectoFinal/src/controlador/controlProcesoCompra.java
+++ ProyectoFinal/src/controlador/controlProcesoCompra.java
@@ -1,3 +1,10 @@
+import java.awt.event.ActionListener;
+import java.util.concurrent.Semaphore;
+
+import javax.swing.JOptionPane;
+
+import java.awt.event.ActionEvent;
+
 public class controlProcesoCompra {
 
     private Eventos evento;
@@ -5,26 +12,44 @@ public class controlProcesoCompra {
     private EventTicketCounter eventCounter;
     private procesoCompra compraProceso;
     private Persistencia p;
-    
-    
-    public controlProcesoCompra(Eventos evento) {
+    private Semaphore semaphoreEventos;
+
+    public controlProcesoCompra(Eventos evento, Semaphore semaphoreEventos) {
         this.evento = evento;
-        p = Persistencia.getInstance();
-        eventCounter = EventTicketCounter.getInstance(evento.getNombreEvento());
-        paymentInterface = PaymentInterface.getInstance();
+        this.p = Persistencia.getInstance();
+        this.eventCounter = EventTicketCounter.getInstance(evento.getNombreEvento());
+        this.paymentInterface = PaymentInterface.getInstance();
+        this.semaphoreEventos = semaphoreEventos;
+
+        // Configurar el botón de compra en EventTicketCounter
+        this.eventCounter.getBuyButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Verifica si hay semaforos de hilo disponible
+                if (semaphoreEventos.availablePermits() > 0) {
+                    // Verifica si esos semaforos se puede adquirir
+                    if (semaphoreEventos.tryAcquire()) {
+                        botonCompraPresionado();
+
+                    } else {
+                        JOptionPane.showMessageDialog(null,
+                                "La cola de ingreso está llena. Por favor, espere su turno.");
+                    }
+                }
+            }
+        });
+
     }
 
-    //Accion del boton de eventoCounter de comprar
-    public void comprar() {
+    public void botonCompraPresionado(){
+        //Obtener los datos ingresados por el panel
+        int cantidadBoletas = Integer.parseInt(eventCounter.getQuantityField().getText());
+        Categorias categoria = (Categorias) eventCounter.getSeatTypeComboBox().getSelectedItem();
+        
+        procesoCompra procesoCompra = new procesoCompra(evento, cantidadBoletas, null, categoria, null);
 
+
+        
     }
-    
-
-    
-
-
-
-
-    
 
 }
