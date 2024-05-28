597a3a1b5fa20aedf280dbe8cf72133c9a4a4a37
nelson.apache <molina123apache@gmal.com>
2024-05-28 01:09:31 -0500

    feat: Agregar funcionalidad de compra en controlador de proceso de compra

--- ProyectoFinal/src/controlador/controlProcesoCompra.java
+++ ProyectoFinal/src/controlador/controlProcesoCompra.java
@@ -1,5 +1,30 @@
 public class controlProcesoCompra {
 
+    private Eventos evento;
+    private PaymentInterface paymentInterface;
+    private EventTicketCounter eventCounter;
+    private procesoCompra compraProceso;
+    private Persistencia p;
+    
+    
+    public controlProcesoCompra(Eventos evento) {
+        this.evento = evento;
+        p = Persistencia.getInstance();
+        eventCounter = EventTicketCounter.getInstance(evento.getNombreEvento());
+        paymentInterface = PaymentInterface.getInstance();
+    }
+
+    //Accion del boton de eventoCounter de comprar
+    public void comprar() {
+
+    }
+    
+
+    
+
+
+
+
     
 
 }
